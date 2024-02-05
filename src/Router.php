547c933e1fba9b9385cfb176465a7a0f68ff3a88
nrzAmirHo3in <nrzamirho3in@gmail.com>
2024-02-06 00:16:01 +0330

    This is test

---
+++ src/Router.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleTools;
+
+class Router
+{
+    function get()
+    {
+
+    }
+    function post()
+    {
+
+    }
+    function run()
+    {
+
+    }
+}
